3252d6d4bf1bbde25c42502a3a9ed5389ff23642
Vinit Kumar <vinitkumar5893@gmail.com>
2023-11-06 13:35:48 +0530

    clock feature enabled in the code...

--- clock.ino
+++ clock.ino
@@ -22,21 +22,60 @@ void MainMenu()
     }
 }
 
-    void setup()
-    {
-        startMillis = millis();
-        lcd_1.setBacklight(1);
-        lcd_1.begin(16, 2);
-        lcd_1.print("hello world");
-        Serial.begin(9600);
-    }
+void setup()
+{
+    startMillis = millis();
+    lcd_1.setBacklight(1);
+    lcd_1.begin(16, 2);
+    lcd_1.print("hello world");
+    Serial.begin(9600);
+}
 
-    void loop()
+void loop()
+{
+    lcd_1.print(startMillis);
+    lcd_1.setCursor(0, 1);
+    MainMenu();
+    lcd_1.print(currentMillis);
+}
+
+void clock(int selecttime)
+{
+    int seconds = 0;
+    int min = 0;
+
+    while (min < selecttime)
     {
-        lcd_1.print(startMillis);
-        lcd_1.setCursor(0, 1);
-        MainMenu();
-        lcd_1.print(currentMillis);
-        
-  
+        if (seconds < 60)
+        {
+            seconds += 1;
+
+            lcd_1.setCursor(0, 0);
+            lcd_1.print(min);
+
+            lcd_1.setCursor(2, 0);
+            lcd_1.print(":");
+
+            lcd_1.setCursor(3, 0);
+            lcd_1.print(seconds);
+            delay(1000);
+        }
+
+        if (seconds == 60)
+        {
+            lcd_1.clear();
+            seconds = 0;
+            min += 1;
+
+            lcd_1.setCursor(0, 0);
+            lcd_1.print(min);
+
+            lcd_1.setCursor(2, 0);
+            lcd_1.print(":");
+
+            lcd_1.setCursor(3, 0);
+            lcd_1.print(seconds);
+            delay(1000);
+        }
     }
+}
